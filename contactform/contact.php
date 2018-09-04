56c629c3cf0f293f06d4648fabfa140adb65c8e1
warrenbrown <warren@katapultmedia.com>
2018-09-03 19:20:32 -0500

    added contact.php for form submission

---
+++ contactform/contact.php
@@ -0,0 +1,19 @@
+<?php
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+
+  $email_from = 'MixingMemories4u Contact Form';
+  $email_subject = "Subject: $subject"
+  $email_body = "Name: $name.\n".
+                "Email: $email.\n".
+                "Message: $message";
+  $to = "warren@katapultmedia.com";
+  $headers = "From: $email_from \r\n";
+  $headers .="Reply-To: $email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+  header("location: index.html");
+ ?>
